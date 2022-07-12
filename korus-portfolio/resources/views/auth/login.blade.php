b0d0f7906cae2d59c25c94a2a85b11c5673a3fd8
Erdélyi Roland <erdelyi.roland99@gmail.com>
2022-07-12 13:00:07 +0200

    Fixed login blade page

--- korus-portfolio/resources/views/auth/login.blade.php
+++ korus-portfolio/resources/views/auth/login.blade.php
@@ -1,34 +1,18 @@
 @extends('master')
 
 @section('content')
-<div class="container">
-
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                                <input id="user" type="hidden" name="user" value="admin">
-
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                    </form>
-</div>
 
 @error('failed')
 {{$message}}
 @enderror
 <div id="login_form_holder">
-    <form action="{{url('/authenticate')}}" method="POST" id="admin_form" class="login_forms">
-        @csrf()
-        <label class="admin_pass">Admin jelszó</label>
+    <form action="{{ route('login') }}" method="POST" id="admin_form" class="login_forms">
+        @csrf
+        <label for="password" class="admin_pass">Admin jelszó</label>
+
         <input type="password" id="admin_pass" name="password">
-        <input type="hidden" name="user" value="admin">
+        <input id="user" type="hidden" name="user" value="admin">
+
         <input type="submit" value="Belépés, mint admin">
     </form>
     <form action="{{url('/')}}" method="POST" id="tag_form" class="login_forms">
