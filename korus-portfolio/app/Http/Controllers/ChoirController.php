6262fb79b491696e026a94cb1c6e38767298df34
Erdélyi Roland <erdelyi.roland99@gmail.com>
2022-08-02 09:03:27 +0200

    Implemented afunction to make old member current and current members old

--- korus-portfolio/app/Http/Controllers/ChoirController.php
+++ korus-portfolio/app/Http/Controllers/ChoirController.php
@@ -19,10 +19,10 @@ class ChoirController extends Controller
         return view('pages/aboutus', compact('current_members', 'old_members'));
     }
 
-    public function editCurrent(){
+    public function editCurrent()
+    {
         $members = Member::all()->where('is_old', 0);
         return view('pages/member/current/edit', compact('members'));
-
     }
 
     public function deleteCurrent($id)
@@ -31,25 +31,36 @@ class ChoirController extends Controller
 
         $path = public_path('uploadfolder/members/');
         $pic = $member->source;
-        unlink($path. $pic);
+        unlink($path . $pic);
         Member::destroy($member->id);
+        return redirect('tag/current/edit');
+    }
+
+    public function transformCurrent($id)
+    {
+        $member = Member::find($id);
+
+        $member['is_old'] = 1;
+
+        $member->save();
+
         return redirect('tag/current/edit');
     }
 
     public function storeCurrent(Request $request)
     {
-        if($request->file('file')){
+        if ($request->file('file')) {
 
             $file = $request->file('file');
-            $destinationPath = public_path().'/uploadfolder/members';
-            $filename = str::random(6) . '_' .($file->getClientOriginalName());
+            $destinationPath = public_path() . '/uploadfolder/members';
+            $filename = str::random(6) . '_' . ($file->getClientOriginalName());
             $uploadSuccess   = $file->move($destinationPath, $filename);
 
-            $image_th = Image::make($destinationPath.'/'.$filename);
-            if($image_th->width() > $image_th->height()){
-                $image_th->crop($image_th->height(),$image_th->height());
-            }else{
-                $image_th->crop($image_th->width(),$image_th->width());
+            $image_th = Image::make($destinationPath . '/' . $filename);
+            if ($image_th->width() > $image_th->height()) {
+                $image_th->crop($image_th->height(), $image_th->height());
+            } else {
+                $image_th->crop($image_th->width(), $image_th->width());
             }
             $image_th->widen(124);
             $image_th->save(null, 90);
@@ -60,15 +71,15 @@ class ChoirController extends Controller
                 'is_old' => 0
             ]);
             return redirect('/tag/current/edit');
-        }else{
+        } else {
             return redirect()->back('success', 'Nem volt file kiválasztva!');
         }
     }
 
-    public function editOld(){
+    public function editOld()
+    {
         $members = Member::all()->where('is_old', 1);
         return view('pages/member/old/edit', compact('members'));
-
     }
 
     public function deleteOld($id)
@@ -77,25 +88,36 @@ class ChoirController extends Controller
 
         $path = public_path('uploadfolder/members/');
         $pic = $member->source;
-        unlink($path. $pic);
+        unlink($path . $pic);
         Member::destroy($member->id);
+        return redirect('tag/old/edit');
+    }
+
+    public function transformOld($id)
+    {
+        $member = Member::find($id);
+
+        $member['is_old'] = 0;
+
+        $member->save();
+
         return redirect('tag/old/edit');
     }
 
     public function storeOld(Request $request)
     {
-        if($request->file('file')){
+        if ($request->file('file')) {
 
             $file = $request->file('file');
-            $destinationPath = public_path().'/uploadfolder/members';
-            $filename = str::random(6) . '_' .($file->getClientOriginalName());
+            $destinationPath = public_path() . '/uploadfolder/members';
+            $filename = str::random(6) . '_' . ($file->getClientOriginalName());
             $uploadSuccess   = $file->move($destinationPath, $filename);
 
-            $image_th = Image::make($destinationPath.'/'.$filename);
-            if($image_th->width() > $image_th->height()){
-                $image_th->crop($image_th->height(),$image_th->height());
-            }else{
-                $image_th->crop($image_th->width(),$image_th->width());
+            $image_th = Image::make($destinationPath . '/' . $filename);
+            if ($image_th->width() > $image_th->height()) {
+                $image_th->crop($image_th->height(), $image_th->height());
+            } else {
+                $image_th->crop($image_th->width(), $image_th->width());
             }
             $image_th->widen(124);
             $image_th->save(null, 90);
@@ -106,7 +128,7 @@ class ChoirController extends Controller
                 'is_old' => 1
             ]);
             return redirect('/tag/old/edit');
-        }else{
+        } else {
             return redirect()->back('success', 'Nem volt file kiválasztva!');
         }
     }
