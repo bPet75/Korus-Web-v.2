9bc29bf084a45e2b6a1780721016e73b14da9964
BSX8GS <k.dorina11@freemail.hu>
2022-08-02 09:16:38 +0200

    responsitity of contact page

--- korus-portfolio/resources/views/pages/contact.blade.php
+++ korus-portfolio/resources/views/pages/contact.blade.php
@@ -10,71 +10,85 @@
 </div>--}}
 <div class="kapcsolat_oszlop">
     <h1>{{trans('pages.kapcsolat1')}}</h1>
-    <div class="kapcsolat_div">
-        <div class="kapcsolat_bal_aldiv">
-            {{trans('pages.kapcsolat2')}}
-        </div>
-        <div class="kapcsolat_bal_aldiv">
-            {{trans('pages.kapcsolat3')}} 
-        </div>
-        <div class="kapcsolat_bal_aldiv">
-            {{trans('pages.kapcsolat4')}}
-        </div>
-        <div class="kapcsolat_bal_aldiv">
-            {{trans('pages.kapcsolat5')}} 
-        </div>
-        <div class="kapcsolat_bal_aldiv">                       
-            {{trans('pages.kapcsolat6')}} 
-        </div>
-        <div class="kapcsolat_bal_aldiv">                       
-            {{trans('pages.kapcsolat6b')}} 
-        </div>
-    </div>
-    <div class="kapcsolat_div">
-        <div class="kapcsolat_jobb_aldiv">
-            3300 Eger, Széchenyi u. 1.
-        </div>
-        <div class="kapcsolat_jobb_aldiv">
-            3300 Eger, Kossuth Lajos utca 8.
-        </div>
-        <div class="kapcsolat_jobb_aldiv">
-            +36 36 517 589
-        </div>
-        <div class="kapcsolat_jobb_aldiv">
-            +36 70 282 4948 
-        </div>
-        <div class="kapcsolat_jobb_aldiv">                       
-            egerfiukorus@gmail.com
-        </div>
-        <div class="kapcsolat_jobb_aldiv">                       
-            Felvételi ügyében kérjük keresse telefonon és e-mailben a kórusvezetőt!
-        </div>
-    </div>
+    <table class="kapcsolat_tablazat">
+        <tr>
+            <td class="kapcsolat_bal_oszlop">
+                {{trans('pages.kapcsolat2')}}
+            </td>
+            <td class="kapcsolat_jobb_oszlop">
+                3300 Eger, Széchenyi u. 1.
+            </td>
+        </tr>
+        <tr>
+            <td class="kapcsolat_bal_oszlop">
+                {{trans('pages.kapcsolat3')}} 
+            </td>
+            <td class="kapcsolat_jobb_oszlop">
+                3300 Eger, Kossuth Lajos utca 8.
+            </td>          
+        </tr>
+        <tr>
+            <td class="kapcsolat_bal_oszlop">
+                {{trans('pages.kapcsolat4')}} 
+            </td>
+            <td class="kapcsolat_jobb_oszlop">
+                +36 36 517 589
+            </td>          
+        </tr>
+        <tr>
+            <td class="kapcsolat_bal_oszlop">
+                {{trans('pages.kapcsolat5')}} 
+            </td>
+            <td class="kapcsolat_jobb_oszlop">
+                +36 70 282 4948
+            </td>          
+        </tr>
+        <tr>
+            <td class="kapcsolat_bal_oszlop">
+                {{trans('pages.kapcsolat6')}} 
+            </td>
+            <td class="kapcsolat_jobb_oszlop">
+                egerfiukorus@gmail.com
+            </td>          
+        </tr>
+        <tr>
+            <td class="kapcsolat_bal_oszlop">
+                {{trans('pages.kapcsolat6b')}} 
+            </td>
+            <td class="kapcsolat_jobb_oszlop">
+                Felvételi ügyében kérjük keresse telefonon és e-mailben a kórusvezetőt!
+            </td>          
+        </tr>
+    </table>
 </div>
 <div class="kapcsolat_oszlop">
     <h1>{{trans('pages.kapcsolat7')}}</h1>
-    <div class="kapcsolat_div">
-        <div class="kapcsolat_bal_aldiv">
-            {{trans('pages.kapcsolat8')}}
-        </div>
-        <div class="kapcsolat_bal_aldiv">
-            {{trans('pages.kapcsolat9')}}
-        </div>
-        <div class="kapcsolat_bal_aldiv">
-            {{trans('pages.kapcsolat10')}}
-        </div>
-    </div>
-    <div class="kapcsolat_div">
-        <div class="kapcsolat_jobb_aldiv">
-            <a class="link" href="http://www.eger.egyhazmegye.hu/">http://www.eger.egyhazmegye.hu/</a>
-        </div>
-        <div class="kapcsolat_jobb_aldiv">
-            <a class="link" href="http://www.szentistvanradio.hu/">http://www.szentistvanradio.hu/</a>
-        </div>
-        <div class="kapcsolat_jobb_aldiv">
-            <a class="link" href="http://www.szentistvanradio.hu/">http://www.szentistvanradio.hu/</a>
-        </div>
-    </div>
+    <table class="kapcsolat_tablazat">
+        <tr>
+            <td class="kapcsolat_bal_oszlop">
+                {{trans('pages.kapcsolat8')}}
+            </td>
+            <td class="kapcsolat_jobb_oszlop">
+                <a class="link" href="http://www.eger.egyhazmegye.hu/">http://www.eger.egyhazmegye.hu/</a>
+            </td>
+        </tr>
+        <tr>
+            <td class="kapcsolat_bal_oszlop">
+                {{trans('pages.kapcsolat9')}} 
+            </td>
+            <td class="kapcsolat_jobb_oszlop">
+                <a class="link" href="http://www.szentistvanradio.hu/">http://www.szentistvanradio.hu/</a>
+            </td>          
+        </tr>
+        <tr>
+            <td class="kapcsolat_bal_oszlop">
+                {{trans('pages.kapcsolat10')}} 
+            </td>
+            <td class="kapcsolat_jobb_oszlop">
+                <a class="link" href="http://www.szentistvanradio.hu/">http://www.szentistvanradio.hu/</a>
+            </td>          
+        </tr>
+    </table>
     <div class="clear"></div>
 </div>
 
